2b61f0dacf2344bf3111940c1826bc12a2a7bff2
unknown <thinsri@gmx.de>
2025-08-13 21:13:31 +0200

    Correct StrOutputParser(), forgot to call it

--- src/generator.py
+++ src/generator.py
@@ -12,8 +12,8 @@ def generate_answer(re_written_query, relevant_chunks):
         st.error("GROQ_API_KEY not found in environment variables.")
         return None
 
-    model_name = "llama-3.3-70b-versatile"
-    llm = ChatGroq(temperature=0.5, model_name=model_name)
+    model_name = "llama3-8b-8192"
+    llm = ChatGroq(temperature=0.5, groq_api_key=groq_api_key, model_name=model_name)
 
     # fetch the chat history
     history = "\n".join([f"{message['role']}: {message['content']}" for message in st.session_state.messages])
@@ -79,7 +79,7 @@ def generate_answer(re_written_query, relevant_chunks):
     })
         | prompt
         | llm
-        | StrOutputParser
+        | StrOutputParser()
     )
 
     # generate a streaming output using the chain
